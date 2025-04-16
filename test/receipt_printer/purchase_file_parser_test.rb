12463c57a2474e0a25944778df136226a7c99d3e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2025-04-16 17:50:03 -0500

    Added some more tests to make sure all given input files are parsing correctly.

--- test/receipt_printer/purchase_file_parser_test.rb
+++ test/receipt_printer/purchase_file_parser_test.rb
@@ -2,34 +2,100 @@ require "test_helper"
 
 describe ReceiptPrinter::PurchaseFileParser do
   describe "when all items are valid" do
-    it "parses the file and returns an Array of ReceiptItems" do
-      purchase_file_parser = ReceiptPrinter::PurchaseFileParser.new(fixture_file("input_1.txt"))
+    describe "test file #1" do
+      it "parses the file and returns an Array of ReceiptItems" do
+        purchase_file_parser = ReceiptPrinter::PurchaseFileParser.new(fixture_file("input_1.txt"))
 
-      items = purchase_file_parser.parse!
+        items = purchase_file_parser.parse!
 
-      _(items.length).must_equal 3
+        _(items.length).must_equal 3
 
-      items.each do |item|
-        _(item).must_be_instance_of ReceiptPrinter::ReceiptItem
+        items.each do |item|
+          _(item).must_be_instance_of ReceiptPrinter::ReceiptItem
+        end
+
+        _(items[0].quantity).must_equal 2
+        _(items[0].name).must_equal "book"
+        _(items[0].imported).must_equal false
+        _(items[0].type).must_equal "book"
+        _(items[0].price).must_equal 12.49
+
+        _(items[1].quantity).must_equal 1
+        _(items[1].name).must_equal "music CD"
+        _(items[1].imported).must_equal false
+        _(items[1].type).must_equal "other"
+        _(items[1].price).must_equal 14.99
+
+        _(items[2].quantity).must_equal 1
+        _(items[2].name).must_equal "chocolate bar"
+        _(items[2].imported).must_equal false
+        _(items[2].type).must_equal "food"
+        _(items[2].price).must_equal 0.85
       end
+    end
 
-      _(items[0].quantity).must_equal 2
-      _(items[0].name).must_equal "book"
-      _(items[0].imported).must_equal false
-      _(items[0].type).must_equal "book"
-      _(items[0].price).must_equal 12.49
+    describe "test file #2" do
+      it "parses the file and returns an Array of ReceiptItems" do
+        purchase_file_parser = ReceiptPrinter::PurchaseFileParser.new(fixture_file("input_2.txt"))
 
-      _(items[1].quantity).must_equal 1
-      _(items[1].name).must_equal "music CD"
-      _(items[1].imported).must_equal false
-      _(items[1].type).must_equal "other"
-      _(items[1].price).must_equal 14.99
+        items = purchase_file_parser.parse!
 
-      _(items[2].quantity).must_equal 1
-      _(items[2].name).must_equal "chocolate bar"
-      _(items[2].imported).must_equal false
-      _(items[2].type).must_equal "food"
-      _(items[2].price).must_equal 0.85
+        _(items.length).must_equal 2
+
+        items.each do |item|
+          _(item).must_be_instance_of ReceiptPrinter::ReceiptItem
+        end
+
+        _(items[0].quantity).must_equal 1
+        _(items[0].name).must_equal "box of chocolates"
+        _(items[0].imported).must_equal true
+        _(items[0].type).must_equal "food"
+        _(items[0].price).must_equal 10.00
+
+        _(items[1].quantity).must_equal 1
+        _(items[1].name).must_equal "bottle of perfume"
+        _(items[1].imported).must_equal true
+        _(items[1].type).must_equal "luxury"
+        _(items[1].price).must_equal 47.50
+      end
+    end
+
+    describe "test file #3" do
+      it "parses the file and returns an Array of ReceiptItems" do
+        purchase_file_parser = ReceiptPrinter::PurchaseFileParser.new(fixture_file("input_3.txt"))
+
+        items = purchase_file_parser.parse!
+
+        _(items.length).must_equal 4
+
+        items.each do |item|
+          _(item).must_be_instance_of ReceiptPrinter::ReceiptItem
+        end
+
+        _(items[0].quantity).must_equal 1
+        _(items[0].name).must_equal "bottle of perfume"
+        _(items[0].imported).must_equal true
+        _(items[0].type).must_equal "luxury"
+        _(items[0].price).must_equal 27.99
+
+        _(items[1].quantity).must_equal 1
+        _(items[1].name).must_equal "bottle of perfume"
+        _(items[1].imported).must_equal false
+        _(items[1].type).must_equal "luxury"
+        _(items[1].price).must_equal 18.99
+
+        _(items[2].quantity).must_equal 1
+        _(items[2].name).must_equal "packet of headache pills"
+        _(items[2].imported).must_equal false
+        _(items[2].type).must_equal "medical"
+        _(items[2].price).must_equal 9.75
+
+        _(items[3].quantity).must_equal 3
+        _(items[3].name).must_equal "boxes of chocolates"
+        _(items[3].imported).must_equal true
+        _(items[3].type).must_equal "food"
+        _(items[3].price).must_equal 11.25
+      end
     end
   end
 
